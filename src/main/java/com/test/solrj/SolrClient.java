6386b92f23f5fa5c27fb8d4cf1b81ebc887ac262
“czbcxy” <k995211314@163.com>
2017-11-20 15:12:14 +0800

    person

--- src/main/java/com/test/solrj/SolrClient.java
+++ src/main/java/com/test/solrj/SolrClient.java
@@ -10,6 +10,9 @@ import org.apache.solr.common.SolrInputDocument;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author chengzhengbo
  * @Date 2017/11/20
@@ -53,24 +56,64 @@ public class SolrClient {
      * 查询
      */
     @Test
-    public void queryTest(){
+    public void queryTest() {
         String query = "*:*";
         SolrQuery solrQuery = new SolrQuery(query);
 
-        solrQuery.set("rows",10);
-
+        //查询语法
 //        solrQuery.set("q","id:carver");
+        //过滤插叙
 //        solrQuery.set("fq","id:carver");
-//        solrQuery.set("sort","id:*");
+        //排序
+//        solrQuery.setSort("id", SolrQuery.ORDER.desc);
+        //分页
+//        int pageSize = 1;
+//        int rows = 222;
+//        int start = rows*(pageSize-1);
+//        solrQuery.setStart(start);
+//        solrQuery.setRows(rows);
+
+        //开启高亮
+        solrQuery.setHighlight(true);
+        solrQuery.setQuery("title:cainiao");
+        //高亮字段
+        solrQuery.addHighlightField("title");
+        //前缀标志
+        solrQuery.setHighlightSimplePre("<span color=red>");
+        //后缀标志
+        solrQuery.setHighlightSimplePost("</span>");
+
+        //用于调试程序
+        System.out.println(solrQuery+"==========");
         try {
+            //相应查询
             QueryResponse response = null;
             response = server.query(solrQuery);
+            //匹配文档
             SolrDocumentList solrDocumentList = response.getResults();
-            System.out.println("总共" +solrDocumentList.getNumFound()+"条记录");
-            for (SolrDocument doc :solrDocumentList ) {
+
+            System.out.println("总共" + solrDocumentList.getNumFound() + "条记录");
+            //从响应中获取高亮信息
+            Map<String, Map<String, List<String>>> map = response.getHighlighting();
+
+            for (SolrDocument doc : solrDocumentList) {
                 System.out.println("id : " + doc.get("id") + "  title : " + doc.get("title"));
+                //取出高亮信息
+                if(map!=null){
+                    //根据id取出高亮信息
+                    String id = (String) doc.get("id");
+                    Map<String, List<String>> idMap = map.get(id);
+                    //取出高亮字段信息
+                    List<String> title = idMap.get("title");
+                    if(title!=null) {
+                        for (int i = 0; i < title.size(); i++) {
+                            System.out.println("title=====" + "  " + title.get(i));
+                        }
+                    }
+
+                }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -97,8 +140,6 @@ public class SolrClient {
         }
 
     }
-
-
 
 
 }
